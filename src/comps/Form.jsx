38b6d58463b6cd92aad6f1f9b1486212a07657ad
Jordan Schulte <email.jordanschulte@gmail.com>
2025-03-31 19:43:51 -0400

    Finished app almost, now working on API portion

--- src/comps/Form.jsx
+++ src/comps/Form.jsx
@@ -1,54 +1,97 @@
-const Form = ({ photoMode }) => {
+import { useState } from "react"
+
+const Form = ({ handlePhotoMode }) => {
+
+  const [photoMode, setPhotoMode] = useState(true);
+  const [formData, setFormData] = useState({
+    img: '',
+    distance: '',
+    lie: '',
+    obstacles: '',
+    wind: '',
+    elevation: '',
+    firmness: '',
+    notes: ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value, type, files } = e.target;
+
+    if(type === 'file') {
+      setFormData((data) => ({
+        ...data,
+        [name]: files[0]
+      }))
+    } else {
+      setFormData((data) => ({
+        ...data,
+        [name]: value
+      }))
+    }
+  }
+
+  const handleMode = (mode) => {
+    mode ? setPhotoMode(true) : setPhotoMode(false)
+  }
+
   return (
     <div>
       <div className="formHeader">
         <h1>Describe Your Shot</h1>
         <p>The more detailed you are, the better advice you will recieve.</p>
       </div>
+
+      <div className="generationType">
+        <button className={`custom-btn ${photoMode ? `chroma-glow-button` : ``}`} onClick={() => setPhotoMode(true)}>Photo Mode</button>
+        <button className={`custom-btn ${photoMode ? `` : `chroma-glow-button`}`} onClick={() => setPhotoMode(false)}>Text Mode</button>
+      </div>
+
        <form>
-        <label>
+        {photoMode ?         <label>
         Upload or Choose a Photo:
             <input
             type="file"
             accept="image/*"
             capture="environment"
+            name="img"
+            onChange={handleChange}
             />
-        </label>
+        </label> : ''}
 
         <label>
         Distance (Yds):
-        <input type="number" placeholder="230 yards"/>    
+        <input type="number" placeholder="230 yards" name="distance" onChange={handleChange} value={formData.distance}/>    
         </label>
 
         <label>
         Lie Type:
-        <input type="text" placeholder="Fairway, rough, bunker, needles, fescue, etc"/>    
+        <input type="text" placeholder="Fairway, rough, bunker, needles, fescue, etc" name="lie" onChange={handleChange} value={formData.lie}/>    
         </label>
 
         <label>
         Obstacles:
-        <input type="text" placeholder="Tree, rock, water"/>    
+        <input type="text" placeholder="Tree, rock, water" name="obstacles" onChange={handleChange} value={formData.obstacles}/>    
         </label>
 
         <label>
         Wind:
-        <input type="text" placeholder="Left to right, light breeze"/>
+        <input type="text" placeholder="Left to right, light breeze" name="wind" onChange={handleChange} value={formData.wind}/>
         <p>Units are not needed just general information.</p>    
         </label>
 
         <label>
         Elevation:
-        <input type="number" placeholder="Elevated green, uphill, flat"/>    
+        <input type="number" placeholder="Elevated green, uphill, flat" name="elevation" onChange={handleChange} value={formData.elevation}/>    
         </label>
 
         <label>
         Course Firmness:
-        <input type="number" placeholder="Elevated green, uphill, flat"/>    
+        <input type="number" placeholder="Elevated green, uphill, flat" name="firmness" onChange={handleChange} value={formData.firmness}/>    
         </label>
 
         <label>
         Additional Notes:
-        <input type="number" placeholder="Elevated green, uphill, flat"/>    
+        <input type="number" placeholder="Elevated green, uphill, flat" name="notes" onChange={handleChange} value={formData.notes}/>    
         </label>
         <button className='chroma-glow-button custom-btn'>Send to Caddy</button>
     </form>
--- src/comps/Home.jsx
+++ src/comps/Home.jsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom"
 
-const Home = ({ photoMode, start }) => {
+const Home = ({ handlePhotoMode }) => {
   return (
     <header id='siteHeader'>
     <div className='headerInner'>
       <h1>The AI Golf Caddy</h1>
       <p>Built to help you play smarter, swing with confidence, and choose the right shot — every time.</p>
       <div className='headerButtons'>
-        <Link to='/form'><button className='chroma-glow-button custom-btn' onClick={photoMode}>Take or Upload Photo</button></Link>
-        <Link to='/form'><button onClick={start} className='custom-btn'>Describe Your Lie</button></Link>
+        <Link to='/form'><button className='chroma-glow-button custom-btn' onClick={handlePhotoMode}>Get Started</button></Link>
       </div>
     </div>
   </header>
