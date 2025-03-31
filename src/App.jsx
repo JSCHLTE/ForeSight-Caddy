7f843c1a3dc13198b8bb0075dbf94ad62d8461d1
Jordan Schulte <email.jordanschulte@gmail.com>
2025-03-31 12:56:35 -0400

    Created Navbar comp

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,19 @@ import './css/base.css';
 import './css/app.css';
 import './css/form.css'
 
+import { Routes, Route } from 'react-router-dom';
+
 import Home from './comps/Home';
 import Form from './comps/Form';
 import { useState } from 'react';
 
 function App() {
 
-  const [start, setStart] = useState(false);
-  const [photo, setPhoto] = useState(false);
-
-  const handlePhotoMode = () => {
-    setPhoto(true);
-    startApp();
-  }
-
-  const startApp = () => {
-    setStart(true);
-  }
-
   return (
-    <div id='siteWrapper'>
-      {start ? <Form /> : <Home photoMode={handlePhotoMode} start={startApp}/>}
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/form' element={<Form />}/>
+    </Routes>
   )
 }
 
---
+++ src/comps/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='navInner'>
+            <div className='navLogo'>
+                <img src='/images/fsc-logo.webp'/>
+            </div>
+            <ul className='navLinks'>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/form'>Form</NavLink></li>
+            </ul>
+            <div className='burger'>
+                <div className='line line1'></div>
+                <div className='line line2'></div>
+                <div className='line line3'></div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
